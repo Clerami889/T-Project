21c9d5a520e1f53612d254d5c1d2226407f9fe5c
Clerami <ImClerami@gmail.com>
2023-06-29 01:59:34 +0700

    Release 1.3

--- src/Parts/Second.jsx
+++ src/Parts/Second.jsx
@@ -3,7 +3,7 @@
 const Second = () => {
   return (
     <div className="box-border shrink-0 w-1px min-h-min flex flex-col justify-start items-center bg-[#4281a4] overflow-hidden flex-[1_0_0%] relative content-center flex-nowrap gap-0 px-5 py-[60px] rounded-[0px_0px_0px_0px]">
-      <div className="flex flex-col items-start gap-y-32 ">
+      <div className="flex flex-col items-start gap-y-16 ">
         <div className="text-white font-firstatas">About</div>
         <div className="text-black font-firstbawah text-[32px]">
           Thea's artistic journey transcends boundaries with an insatiable
